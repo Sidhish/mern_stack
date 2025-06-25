24c5f6d1c73fbf8af7719b3586c1003d0eeae7c5
sidhish <sidhish.p@gmail.com>
2025-06-25 20:46:11 +0530

    #react

---
+++ my-app/src/components/Greet.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function Greet(props) {
+  const { studentName, college, ...prop } = props;
+  return (
+    <h1>
+      Hello {studentName}, {college}, {prop.branch}
+    </h1>
+  );
+}
+
+export default Greet;
