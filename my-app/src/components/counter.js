bf80711f9f0f0f0877cf90dea8b562c3ef887605
sidhish <sidhish.p@gmail.com>
2025-06-27 09:30:27 +0530

    #Mongodb

--- my-app/src/components/counter.js
+++ my-app/src/components/counter.js
@@ -1,14 +1,40 @@
 import React, {useState} from "react"
 
 function Counter(){
-    const [name,setName] = useState("LPU");
-    function changeName(){
-        setName("Students");
+    const [timer,setTimer] = useState(0);
+    const [interval,setInter] = useState(null);
+    // function changeName(){
+    //     setName(name+1);
+    // }
+    // function change(){
+    //     setName(name-1); 
+    // }
+    // let interval = null; 
+    function startTimer(){
+        const i = setInterval(() =>{
+            setTimer ((prev) => prev+1);
+        },1000);
+
+        setInter(i);
     }
+    function stopTimer(){
+        clearInterval(interval);
+        setInter(null);
+    }
+    function resetTimer(){
+        setTimer(0);
+    }
+    
     return (
-        <main>
-            <h1>Hello {name}</h1>
-            <button onClick={changeName}>Change</button>
+         <main style={{textAlign: "center"}}>
+            <h1>Timer: {timer} seconds</h1>
+            <div>
+            <button onClick={startTimer}>Start</button>
+            &nbsp; &nbsp;
+            <button onClick={stopTimer}>Stop</button>
+            &nbsp; &nbsp;
+            <button onClick={resetTimer}>Reset</button>
+            </div>
         </main>
     )
 }
